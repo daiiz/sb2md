c4fe1973f80f1c050594e2e0439009cfb0135838
daiiz <da.iz.mg@gmail.com>
2017-07-08 00:22:40 +0900

    rm sb2tex/

--- sb2tex/create_pdf.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-echo "run"
-platex raw.tex
-dvipdfmx raw.dvi
-open raw.pdf
--- sb2tex/create_pdf_full.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-echo "run"
-extractbb gyazos/*.png
-extractbb gyazos/*.jpg
-platex raw.tex
-dvipdfmx raw.dvi
-open raw.pdf
